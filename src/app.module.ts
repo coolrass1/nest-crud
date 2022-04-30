f90f04688fde616ae861f4114e329dfe93edd176
unknown <coolrass1@aol.co.uk>
2022-04-30 20:33:52 +0200

    crud auth2  docker  commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountsModule } from './accounts/accounts.module';
@@ -8,9 +8,22 @@ import { PostsController } from './posts/posts.controller';
 import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
 import { AuthsModule } from './auths/auths.module';
+import configuration from './config/configuration';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot(),MongooseModule.forRoot('mongodb://localhost/nest'),AccountsModule, PostsModule, UsersModule, AuthsModule],
+  imports: [
+
+
+    ConfigModule.forRoot(),
+MongooseModule.forRoot( `mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASSWORD}@cluster0.vgtpa.mongodb.net/${process.env.MONGO_ATLAS_DB}?retryWrites=true&w=majority`),
+ 
+    AccountsModule,
+    PostsModule,
+    UsersModule,
+    AuthsModule,
+    
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
